034e9ae517f88c49c85ee5a51980445dd5e7107e
vblana <rovlade@yahoo.com>
2018-01-08 23:50:13 +0200

    Added doctor navbar Added my-patients page - work in progress

---
+++ UID PROIECT/uid-project/src/app/doctor-page-navbar/doctor-page-navbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-page-navbar',
+  templateUrl: './doctor-page-navbar.component.html',
+  styleUrls: ['./doctor-page-navbar.component.css']
+})
+export class DoctorPageNavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
