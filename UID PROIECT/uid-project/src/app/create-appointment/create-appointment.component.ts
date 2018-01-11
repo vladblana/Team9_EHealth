8b80804571b21dadeafb8681375a962f7a695af9
DESKTOP-A82S5SS\Calin <calin.timbus@gmail.com>
2018-01-11 18:31:54 +0200

    1)Create appointment 90% finished --small functionality still needs to be implemented. 2)Check out the dialog-overview-example. It provides a dialog sample and you can you it whether you need it. 3)Check out the form validations from the create appointment. You may need it when implementing your own tasks.

--- UID PROIECT/uid-project/src/app/create-appointment/create-appointment.component.ts
+++ UID PROIECT/uid-project/src/app/create-appointment/create-appointment.component.ts
@@ -1,38 +1,91 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Injectable} from '@angular/core';
+import {DoctorService} from "../service/DoctorService";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {PatientService} from "../service/PatientService";
+import {DialogOverviewExampleDialogComponent} from "../dialog-overview-example-dialog/dialog-overview-example-dialog.component";
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
 
 @Component({
   selector: 'app-create-appointment',
   templateUrl: './create-appointment.component.html',
   styleUrls: ['./create-appointment.component.css']
 })
+@Injectable()
 export class CreateAppointmentComponent implements OnInit {
   hours = [7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
   minutes = ["00", "30"];
+  doctors = [];
+  doctorNames = <any>[];
+  dateFormGroup: FormGroup;
+  timeFormGroup: FormGroup;
+  doctorNameFormGroup: FormGroup;
+  optionRadioValues = ['Routine', 'Urgent'];
+  pickedStatus: string;
+
 
   filteredHours() {
-    return this.hours.filter(e => e % 2 == 0);
+    return this.hours.filter(e => e % 2 === 0);
   }
 
   myFilter = (d: Date): boolean => {
     const chosenDay = d.getDate();
     const chosenMonth = d.getMonth();
     const sundays = d.getDay();
-    let currentDay = new Date().getDate();
-    let currentMonth = new Date().getMonth();
+    const currentDay = new Date().getDate();
+    const currentMonth = new Date().getMonth();
     console.log('Day of the month is: ', currentDay);
     if (chosenMonth === currentMonth) {
-      return chosenDay > currentDay! && sundays != 0;
+      return chosenDay > currentDay! && sundays !== 0;
     }
     else {
-      return sundays != 0;
+      return sundays !== 0;
     }
   };
 
 
-  constructor() {
+  constructor(private doctorService: DoctorService, private patientService: PatientService, private formBuilder: FormBuilder, private dialog: MatDialog) {
+    this.doctors = doctorService.initializeDoctors();
+    this.retrieveDoctorNames();
   }
 
   ngOnInit() {
+    this.dateFormGroup = this.formBuilder.group({
+      dateCtrl: ['', Validators.required]
+    });
+    this.timeFormGroup = this.formBuilder.group({
+      timeHoursFormCtrl: ['', Validators.required],
+      timeMinutesFormCtrl: ['', Validators.required]
+    });
+    this.doctorNameFormGroup = this.formBuilder.group({
+      doctorNameFormCtrl: ['', Validators.required]
+    });
   }
 
+  retrieveDoctorNames() {
+    this.doctors.forEach((doctor, index) => {
+      console.log(this.pickedStatus);
+      this.doctorNames.push(doctor.firstName + ' ' + doctor.lastName);
+    });
+  }
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DialogOverviewExampleDialogComponent, {
+      width: '250px',
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+      if (result === true) {
+        console.log('Patient chose to send appointment');
+      } else {
+        console.log('Patient still modifying stuff');
+      }
+    });
+  }
+
+
+  onRequestAppointment() {
+    console.log('Appointment requested');
+    this.openDialog();
+  }
 }
